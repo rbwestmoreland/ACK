9963db4689003aa1be8d1166ba3caeecd81df60d
Bates Westmoreland <rbwestmoreland@gmail.com>
2013-03-23 10:43:58 -0400

    added Ack.Infrastructure project
    - added TinyIoC package

---
+++ src/Ack.Infrastructure/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ack.Infrastructure")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: Guid("c56fa551-9237-4bcf-a719-58a6f9fe4edc")]
