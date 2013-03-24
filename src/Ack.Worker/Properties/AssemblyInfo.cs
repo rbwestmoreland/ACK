fb4af817d3732bc37d7fee954f8ed9699c2c3cc2
Bates Westmoreland <rbwestmoreland@gmail.com>
2013-03-24 17:42:41 -0400

    added Ack.Worker project

---
+++ src/Ack.Worker/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ack.Worker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: Guid("3cff98e8-793b-4e78-93be-7a685d7b3c09")]
