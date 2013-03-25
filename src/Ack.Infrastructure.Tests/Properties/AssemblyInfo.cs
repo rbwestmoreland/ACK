77549bb8268e3bd7b0b9df409adb8401c431d1a8
Bates Westmoreland <rbwestmoreland@gmail.com>
2013-03-24 21:03:41 -0400

    added unit test projects
    - added Ack.Infrastructure.Tests
    - added Ack.Worker.Tests

---
+++ src/Ack.Infrastructure.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ack.Infrastructure.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: Guid("533c32ab-b942-4546-9c89-cdc72ba46b9a")]
---
+++ src/Ack.Worker.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ack.Worker.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: Guid("a888f86c-82ec-494e-82af-5149d15cc944")]
